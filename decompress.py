62fdff82d346ace438d453b9c63d1c9160ef9a1e
Caroline Kaufman <carolinekaufman97@gmail.com>
2018-01-03 18:56:28 -0500

    nonrecursive case works

--- decompress.py
+++ decompress.py
@@ -4,5 +4,39 @@ From Google's Technical Development Guide: Advanced Path
 String Decompression
 
 Goal: Decompress compressed strings written in the form
-        num_repeats[string_to_repeat]
+    num_repeats[string_to_repeat]
 """
+
+def decompress(s):
+    """
+    Given a string of the form num_repeats[string_to_repeat], 
+    return the decompressed string
+    """
+    decompressed = ""
+    i = 0
+    while i < len(s):
+        n = 1
+        num = ""
+        to_repeat = ""
+        if s[i].isdigit():
+            while s[i].isdigit() and i < len(s):
+                num += s[i]
+                i += 1
+            n = int(num) # get number of times to repeat
+        if i < len(s) and s[i] == "[": # do not include [ in repeated string
+            i += 1
+        if i < len(s) and s[i].isdigit():
+            to_repeat += decompress() #substring of s from [ to ]
+        else:   # next char is letter
+            while i < len(s) and s[i].isalpha():
+                to_repeat += s[i]
+                i += 1
+        decompressed += to_repeat * n
+        i += 1
+    return decompressed
+
+def main():
+    print decompress("2[3[a]b]")
+
+if __name__ == '__main__':
+    main()
